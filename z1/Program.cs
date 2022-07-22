3dfd6659997eb3ea160d2444650016c23f5fd916
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-07-22 14:56:42 +0500

    Homework seminar 9

---
+++ z1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Factorial(int N)
+{
+    if(N >= 1)
+    {
+        return N * (Factorial(N - 1));
+    }
+
+    return 1;
+}
+
+Console.Write(Factorial(4));
---
+++ z2/Program.cs
@@ -0,0 +1,11 @@
+﻿int Sum(int M, int N)
+{
+    if(M == N)
+    {
+        return M;
+    }
+
+    return N + Sum(M, N - 1);
+}
+
+Console.Write(Sum(1,15));
